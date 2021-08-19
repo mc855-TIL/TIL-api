7455d847266d285022a36327b4944ecc57860717
leokoike <leokoike@gmail.com>
2021-08-19 14:48:26 -0300

    começo do projeto em Django Rest Framework

---
+++ api/server/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=30)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=11, blank=True, null=True)
+    data_nascimento = models.DateField()
+
+class Produto(models.Model):
+    
---
+++ api/server/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+
+
+def user(request):
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        pass
