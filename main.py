29f267b348a6ed5397474bbc0423a8a0626571c2
leokoike <leokoike@gmail.com>
2021-08-18 23:56:22 -0300

    troca de framework

--- main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI(
-    title="Troca de insumos de laboratórios - API",
-    description="API utilizada para criar o servidor do site de Troca de insumos de laboratórios.",
-)
