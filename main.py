bd5b42abaabc9750241a9db4477d38dd44c98b4a
Leonardo Koike <leokoike@gmail.com>
2021-09-11 19:00:33 -0300

    adiciona primeira rota

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-# import pydantic
-app = FastAPI()
+from app.api.route.oferta_route import app as oferta_app
 
-db = []
+app = FastAPI(
+    title="Projeto MC855 - API Troca de insumos de laboratório.",
+    description="API para realizar a troca de insumos de laboratório.",
+    version="1.0",
+)
 
-@app.get('/')
-def index():
-    return {'key': 'value'}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+app.include_router(oferta_app, tags=["Ofertas"])
